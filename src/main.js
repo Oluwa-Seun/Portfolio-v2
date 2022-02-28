08ac7968a67bcfc33677e72a174dac5dff73f6af
Oluwa-Seun <oluwaseunolubiyione@gmail.com>
2022-02-28 02:45:02 +0100

    🚀ServiceWorker

--- src/main.js
+++ src/main.js
@@ -1,7 +1,8 @@
-import Vue from 'vue'
-import App from './App.vue'
-import './index.css'
+import Vue from 'vue';
+import App from './App.vue';
+import './index.css';
 import VueMeta from "vue-meta";
+import './registerServiceWorker';
 
 Vue.use(VueMeta)
 
@@ -9,5 +10,5 @@ Vue.config.productionTip = false
 Vue.config.silent = true
 
 new Vue({
-  render: h => h(App),
+    render: h => h(App),
 }).$mount('#app')
